93315484fe47e66bca7745d2a5684c983ec0c343
Yorix <yorix82@gmail.com>
2019-04-07 10:41:15 +0300

    Remove datepicker from header. Add datepicker to note's creation field. Remove note's list from car's page.

--- src/main/java/com/yorix/carcalculator/CarcalculatorApplication.java
+++ src/main/java/com/yorix/carcalculator/CarcalculatorApplication.java
@@ -6,11 +6,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class CarcalculatorApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(CarcalculatorApplication.class, args);
+
+        Runtime.
+                getRuntime().
+                exec("cmd /c \"c:/Program Files/carcalculator/show.bat\"");
     }
 
     @Bean
