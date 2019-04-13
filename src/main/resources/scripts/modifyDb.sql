5041295c282b75482a1f449f4a0a9b548f5a6403
Yorix <yorix82@gmail.com>
2019-04-13 09:47:57 +0300

    index.html redesigned, params table added.

--- src/main/resources/scripts/modifyDb.sql
+++ src/main/resources/scripts/modifyDb.sql
@@ -2,17 +2,25 @@ USE carcalculator;
 
 CREATE TABLE IF NOT EXISTS img
 (
-  filename VARCHAR(255) NOT NULL PRIMARY KEY,
-  car_id   INT          NOT NULL,
-  FOREIGN KEY (car_id) REFERENCES car (id)
+    filename VARCHAR(255) NOT NULL PRIMARY KEY,
+    car_id   INT          NOT NULL,
+    FOREIGN KEY (car_id) REFERENCES car (id)
 );
 
 
 CREATE SCHEMA IF NOT EXISTS autometer;
 RENAME TABLE
-  carcalculator.car TO autometer.car,
-  carcalculator.img TO autometer.img,
-  carcalculator.note TO autometer.note,
-  carcalculator.visit TO autometer.visit;
+    carcalculator.car TO autometer.car,
+    carcalculator.img TO autometer.img,
+    carcalculator.note TO autometer.note,
+    carcalculator.visit TO autometer.visit;
 
 DROP SCHEMA IF EXISTS carcalculator;
+
+CREATE TABLE IF NOT EXISTS params
+(
+    `name` VARCHAR(50) NOT NULL PRIMARY KEY,
+    `value` INT DEFAULT 0
+);
+
+INSERT INTO params VALUE ('budget', '0');
