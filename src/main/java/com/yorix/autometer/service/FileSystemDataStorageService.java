d188c0a95744035cf10ccca87d6dd3bd053c471e
Yorix <yorix82@gmail.com>
2019-05-06 16:57:40 +0300

    Reading data from files is in deployment.

--- src/main/java/com/yorix/autometer/service/FileSystemDataStorageService.java
+++ src/main/java/com/yorix/autometer/service/FileSystemDataStorageService.java
@@ -1,19 +1,19 @@
 package com.yorix.autometer.service;
 
 import com.yorix.autometer.config.AppProperties;
-import com.yorix.autometer.model.CarJsonDTO;
-import com.yorix.autometer.model.ImgJsonDTO;
-import com.yorix.autometer.model.NoteJsonDTO;
+import com.yorix.autometer.model.*;
 import com.yorix.autometer.storage.CarRepository;
 import com.yorix.autometer.storage.ImgRepository;
 import com.yorix.autometer.storage.NoteRepository;
 import com.yorix.autometer.storage.ParamRepository;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
 import org.springframework.stereotype.Service;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -25,7 +25,8 @@ public class FileSystemDataStorageService implements DataStorageService {
     private final NoteRepository noteRepository;
     private final ParamRepository paramRepository;
     private final ImgRepository imgRepository;
-    private final ImageStorageService imageStorageService;
+    private final JdbcTemplate jdbcTemplate;
+    private final String rootLoc;
     private final String dbLoc;
 
     @Autowired
@@ -33,13 +34,14 @@ public class FileSystemDataStorageService implements DataStorageService {
                                         NoteRepository noteRepository,
                                         ParamRepository paramRepository,
                                         ImgRepository imgRepository,
-                                        ImageStorageService imageStorageService,
+                                        JdbcTemplate jdbcTemplate,
                                         AppProperties properties) {
         this.carRepository = carRepository;
         this.noteRepository = noteRepository;
         this.paramRepository = paramRepository;
         this.imgRepository = imgRepository;
-        this.imageStorageService = imageStorageService;
+        this.jdbcTemplate = jdbcTemplate;
+        this.rootLoc = properties.getRootLocation();
         this.dbLoc = properties.getDbBackupLocation();
     }
 
@@ -70,6 +72,74 @@ public class FileSystemDataStorageService implements DataStorageService {
 
     @Override
     public void read() {
+        String carsStr = getString(dbLoc + "cars.json");
+        String notesStr = getString(dbLoc + "notes.json");
+        String paramsStr = getString(dbLoc + "params.json");
+        String imgsStr = getString(dbLoc + "imgs.json");
 
+        String initDB = "";
+        try (InputStream inputStream = getClass().getResourceAsStream("/scripts/initDB.sql")) {
+            initDB = new String(inputStream.readAllBytes()).replaceAll("\\s+", " ");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        jdbcTemplate.execute(initDB); //TODO
+
+        JSONArray jsonArray = new JSONArray(carsStr);
+        for (int i = 0, length = jsonArray.length(); i < length; i++) {
+            Car car = new Car();
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            car.setId(jsonObject.getInt("id"));
+            car.setMake(jsonObject.getString("make"));
+            car.setModel(jsonObject.getString("model"));
+            car.setImgFilename(jsonObject.getString("imgFilename"));
+
+            carRepository.save(car);
+        }
+
+        jsonArray = new JSONArray(notesStr);
+        for (int i = 0, length = jsonArray.length(); i < length; i++) {
+            Note note = new Note();
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            note.setId(jsonObject.getInt("id"));
+            note.setDescription(jsonObject.getString("description"));
+            note.setValue(jsonObject.getDouble("value"));
+            note.setDate(jsonObject.getString("date"));
+            note.setCar(carRepository.getOne(jsonObject.getInt("carId")));
+
+            noteRepository.save(note);
+        }
+
+        jsonArray = new JSONArray(paramsStr);
+        for (int i = 0, length = jsonArray.length(); i < length; i++) {
+            Param param = new Param();
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+            param.setName(jsonObject.getString("name"));
+            param.setValue(jsonObject.getDouble("value"));
+
+            paramRepository.save(param);
+        }
+
+        jsonArray = new JSONArray(imgsStr);
+        for (int i = 0, length = jsonArray.length(); i < length; i++) {
+            Img img = new Img();
+            JSONObject jsonObject = jsonArray.getJSONObject(i); //TODO
+            ImgJsonDTO imgJsonDTO = (ImgJsonDTO) jsonArray.get(i);
+            System.out.println(imgJsonDTO.getFilename());
+        }
+    }
+
+    private String getString(String path) {
+        File file = new File(path);
+        String text = "";
+        try (InputStream inputStream = new FileInputStream(file)) {
+            text = new String(inputStream.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }
