f6a398f3903c84f6374d46b61112f9991737fad0
Yorix <yorix82@gmail.com>
2020-02-16 09:15:23 +0200

    removal of secret in separate properties

--- src/main/java/com/yorix/autometer/AutometerApplication.java
+++ src/main/java/com/yorix/autometer/AutometerApplication.java
@@ -2,8 +2,10 @@ package com.yorix.autometer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:constants.properties")
 public class AutometerApplication {
     public static void main(String[] args) {
         SpringApplication.run(AutometerApplication.class, args);
