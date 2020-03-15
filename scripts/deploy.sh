7f34e275ec0fa494a4530a086c4a0ea4cfa2efad
Yorix <yorix82@gmail.com>
2020-03-15 20:53:39 +0200

    relocated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,11 @@ echo "Copy files..."
 
 scp -i ~/.ssh/gcloud_autometer \
 	target/autometer-1.1.jar \
-	autometer@35.229.125.16:/home/autometer/
+	autometer@35.196.160.86:/home/autometer/
 
 echo "Restart server..."
 
-ssh -i ~/.ssh/gcloud_autometer autometer@35.229.125.16 << EOF
+ssh -i ~/.ssh/gcloud_autometer autometer@35.196.160.86 << EOF
 
 pgrep java | xargs kill -9
 nohup java -jar autometer-1.1.jar > log.txt &
