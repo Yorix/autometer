981652f0df635a5d6babcc7aaefcf6cc8a4816cc
yorix <yorix82@gmail.com>
2021-02-15 15:23:20 +0200

    migrated

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,11 +6,11 @@ echo "Copy files..."
 
 scp -i ~/.ssh/gcloud_autometer \
 	target/autometer-1.1.jar \
-	autometer@35.196.160.86:/home/autometer/
+	autometer@34.65.213.174:/home/autometer/
 
 echo "Restart server..."
 
-ssh -i ~/.ssh/gcloud_autometer autometer@35.196.160.86 << EOF
+ssh -i ~/.ssh/gcloud_autometer autometer@34.65.213.174 << EOF
 
 pgrep java | xargs kill -9
 nohup java -jar autometer-1.1.jar > log.txt &
